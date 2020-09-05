ed1e1937874534a43cb50e2df938f102af970ed5
Oliver <oliver@tapirens.se>
2020-09-05 11:14:12 +0200

    feat(PDFController): update to use template and data objects

--- mysilverstripeapp/app/src/PDFController.php
+++ mysilverstripeapp/app/src/PDFController.php
@@ -3,15 +3,46 @@
 namespace mynamespace;
 
 use Dompdf\Dompdf;
+use Dompdf\Options;
 use SilverStripe\Control\Controller;
 
 class PDFController extends Controller {
 
+  private static $url_handlers = [
+    '$id' => 'index',
+  ];
+
+  // Changes status code of response and sets an error message
+  private function error($code, $msg) {
+    $this->getResponse()->setStatusCode($code);
+    $this->getResponse()->addHeader('Content-Type', 'text/plain');
+    return $msg;
+  }
+
   public function index($request) {
 
-    $dompdf = new Dompdf();
+    // Check for ID
+    if (!isset($request->params()['id'])) {
+      return $this->error(400, 'No id supplied');
+    }
 
-    $dompdf->loadHtml('<h1>It works!</h1>');
+    // Get MyDataObject by id
+    $objectID = $request->params()['id'];
+    $myObject = MyDataObject::get_by_id($objectID);
+
+    // Check if object exists
+    if (!$myObject) {
+      return $this->error(404, 'Object not found');
+    }
+
+    // Allow access to remote resources such as images
+    $options = new Options();
+    $options->set('isRemoteEnabled', true);
+
+    $dompdf = new Dompdf($options);
+
+    // Process MyPDF template using MyDataObject and load its html
+    $dompdf->loadHtml($myObject->renderWith('MyPDF'));
 
     // (Optional) Setup the paper size and orientation
     $dompdf->setPaper('A4', 'portrait');
